e92ab0617ea75c3cb63cb760ae1dd5e364a8a375
Alexey Fadeev <defident39@gmail.com>
2018-11-13 23:45:28 +0300

    Simplify the code by using a common neural network interface

--- app/neural_network.py
+++ app/neural_network.py
@@ -7,14 +7,17 @@ OUTPUT_SIZE = 10
 
 class NeuralNetwork:
     def __init__(self, hidden_layer_size: int):
-        self._hidden_layer = HiddenNeuronLayer(hidden_layer_size, INPUT_SIZE)
-        self._output_layer = OutputNeuronLayer(OUTPUT_SIZE, hidden_layer_size)
+        self._layers = [
+            HiddenNeuronLayer(hidden_layer_size, INPUT_SIZE),
+            OutputNeuronLayer(OUTPUT_SIZE, hidden_layer_size)
+        ]
 
     def _feed_forward(self, inputs: list):
-        hidden_layer_outputs = self._hidden_layer.feed_forward(inputs)
-        output_layer_outputs = self._output_layer.feed_forward(hidden_layer_outputs)
+        current_inputs = inputs
+        for layer in self._layers:
+            current_inputs = layer.feed_forward(current_inputs)
 
-        return output_layer_outputs
+        return current_inputs
 
     def train(self, training_inputs: list, training_outputs: list):
         self._feed_forward(training_inputs)
