1c06fe03401da1ae00ad4a4bdd04762e3378ee7f
Alexey Fadeev <alexey.fadeev@intel.com>
2018-11-18 17:29:07 +0300

    Added total error calculation

--- app/neural_network.py
+++ app/neural_network.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from app.neuron_layers.hidden_layer import HiddenNeuronLayer
 from app.neuron_layers.output_layer import OutputNeuronLayer
 
@@ -6,11 +8,12 @@ OUTPUT_SIZE = 10
 
 
 class NeuralNetwork:
-    def __init__(self, hidden_layer_size: int):
+    def __init__(self, hidden_layer_size: int, learning_rate: float):
         self._layers = [
             HiddenNeuronLayer(hidden_layer_size, INPUT_SIZE),
             OutputNeuronLayer(OUTPUT_SIZE, hidden_layer_size)
         ]
+        self._learning_rate = learning_rate
 
     def _feed_forward(self, inputs: list):
         current_inputs = inputs
@@ -19,5 +22,15 @@ class NeuralNetwork:
 
         return current_inputs
 
+    def _cross_entropy_error(self, computed: list, targets: list):
+        total_error = 0
+        for neuron_output, expected_output in zip(computed, targets):
+            error = expected_output * np.log(neuron_output) + (1 - expected_output) * np.log(1 - neuron_output)
+            total_error += error
+
+        return -total_error/len(computed)
+
     def train(self, training_inputs: list, training_outputs: list):
-        self._feed_forward(training_inputs)
+        output = self._feed_forward(training_inputs)
+        total_error = self._cross_entropy_error(output, training_outputs)
+        pass
